ea1ee133212d3ff02601b8c4bb88c1d5160d1919
henry.shen@outlook.com <henry.shen@outlook.com>
2021-09-07 22:52:38 -0400

    new song dropped

---
+++ jimothyInspired.py
@@ -0,0 +1,18 @@
+#melodies 
+p2 >> pluck([3,0,4,5], dur = [1/4, 1/4, 1/2])
+p2 >> pluck(var([3,0,1,5,2], [4,2]))
+
+p2 >> pluck([3,0,4,5], dur = [1/4, 1/4, 1/4, 1/4])
+p2 >> pluck(var([7,4,3,1],4))
+
+p3 >> pluck(var([3,0,5,4],4), dur=[1,1,1,1/4,3/4])
+
+# harmonies
+p1 >> keys(var([(-4,-2, 0), (1, 3, 5)], 4), dur=[3/4, 3/4, 3/4, 3/4, 1/2, 1/2])
+d1 >> play('X  Xo ++', amp=[2, 1], sample=6)
+f1 >> swell([0,1], sus=4, dur=4, amp=0.5, root=2)
+#>  >   >   >   > >
+#rlrl rlrl rlrl rlrl
+print(Clock.bpm)
+Clock.bpm = 100
+
